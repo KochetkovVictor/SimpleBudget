cc5f68c53a68f2d48b6a1d46d26e13803406e657
KochetkovVictor <che18@yandex.ru>
2017-03-21 11:27:38 +0300

    21.03.17 11:26

--- src/main/java/ru/simplebudget/controller/purse/PurseController.java
+++ src/main/java/ru/simplebudget/controller/purse/PurseController.java
@@ -2,22 +2,17 @@ package ru.simplebudget.controller.purse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import org.springframework.web.servlet.ModelAndView;
-import ru.simplebudget.model.common.Purse;
 import ru.simplebudget.service.purse.PurseServiceImpl;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-
 @Controller
-@RequestMapping(value="/")
+@RequestMapping(value = "/purses")
 public class PurseController {
 
     @Autowired
@@ -25,17 +20,10 @@ public class PurseController {
     PurseServiceImpl service;
 
     @RequestMapping(method = RequestMethod.GET)
-    public /*String*/ ModelAndView getAll(/*Model model*/)
-    {
-        //model.addAttribute("purseList",service.getAll());
-        //ModelAndView mav=new ModelAndView("index", "totalAmount", service.getTotalAmount());
-       // mav.addObjects()new ModelAndView("index","purseList",service.getAll());
-       /* model.addAttribute("purseList",service.getAll());
-        model.addAttribute("totalAmount",service.getTotalAmount());*/
-        Map<String, Object> modelMap=new HashMap<>();
+    public ModelAndView getAll() {
+        Map<String, Object> modelMap = new HashMap<>();
         modelMap.put("purseList", service.getAll());
         modelMap.put("totalAmount", service.getTotalAmount());
-
-        return /*"index"*/new ModelAndView("index", modelMap);
+        return new ModelAndView("purses", modelMap);
     }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -1,33 +1,11 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
-<title>Simple Budget</title>
-<style>
-    .enable {visibility: visible;}
-    .disable{visibility: collapse; border: hidden;}
-    .hidden {visibility: hidden;}
-</style>
+<head>
+    <title>Simple Budget v.0.1</title>
+</head>
 <body>
-<h2>Simple Budget v.0.1</h2>
-
-<!--link href="color.css" rel="stylesheet" type="text/css"  property="class"-->
-<a href="purses?action=add">Add Purse</a>
-<table border="1" cellpadding="8" cellspacing="0">
-    <thead>
-    <tr>
-        <th>Description</th>
-        <th>Amount</th>
-    </tr>
-    </thead>
-    <c:forEach items="${purseList}" var="purse">
-        <jsp:useBean id="purse" scope="page" type="ru.simplebudget.model.common.Purse"/>
-        <tr class="${purse.active ? 'enable' : 'disable'}">
-            <td>${purse.description}</td>
-            <td>${purse.amount}</td>
-        </tr>
-    </c:forEach>
-    <td class='disable'/>
-    <td>${totalAmount}</td>
-</table>
+<a href="purses">Purses</a>
+<a href="incomes">Incomes</a>
+<a href="receiptes">Reseiptes</a>
 </body>
 </html>
