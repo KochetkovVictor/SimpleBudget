0e95dfbdaa01652ed6edea17a3d92771f12966b3
KochetkovVictor <che18@yandex.ru>
2017-05-10 12:29:20 +0300

    10.05.17 12:26 start to implement security.add user class

--- src/main/java/ru/simplebudget/model/user/User.java
+++ src/main/java/ru/simplebudget/model/user/User.java
@@ -1,6 +1,12 @@
 package ru.simplebudget.model.user;
 
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.cglib.core.Local;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.metamodel.StaticMetamodel;
@@ -13,11 +19,25 @@ public class User {
 
     @Id
     private Long id;
+    @Column(name="nickname", nullable = false, unique = true)
     private String nickName;
+    @Column(name = "password", nullable = false)
+    @NotEmpty
+    @Length(min = 5)
     private String password;
+    @Column(name = "dayofbirth", columnDefinition = "timestamp default now()")
     private LocalDate dateOfBirth;
+    @Column
     private String firsName;
+    @Column
     private String LastName;
+    @Column(name = "email", nullable = false, unique = true)
+    @Email
+    @NotEmpty
+    private String email;
+
+    @Column(name = "registered", columnDefinition = "timestamp default now()")
+    private LocalDate registered;
 
     public Long getId() {
         return id;
@@ -65,5 +85,19 @@ public class User {
 
     public void setLastName(String lastName) {
         LastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public LocalDate getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(LocalDate registered) {
+        this.registered = registered;
     }
 }
